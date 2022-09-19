b9994a426ec1c718029238dbf91920bb25b55e72
Onga22 <107556703+Onga22@users.noreply.github.com>
2022-09-19 15:40:08 -0300

    Add files via upload

---
+++ JavaBasicoEjer3.java
@@ -0,0 +1,18 @@
+
+package domain;
+
+
+public class JavaBasicoEjer3 {
+    public static void main(String[] args) {
+         
+        String [] nombres = {"Jorge", "Tini", "jonathan","Larreta"};
+        
+        for (String nombre : nombres) {
+            System.out.print(nombre +" ");
+            
+        }
+    
+        }
+       
+    }
+
